0fd21d4e471e6d11f408ae3058d7c4be4e663ac9
fuqiang <fuqiang@coding.net>
2021-03-01 14:03:48 +0800

    关闭ssl模式

--- xtrabakcup.sh
+++ xtrabakcup.sh
@@ -10,5 +10,5 @@ TARGET_DIR=
 
 # 开始备份
 echo "xtrabackup开始备份..."
-xtrabackup --backup --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --target-dir=$TARGET_DIR
+xtrabackup --backup --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --ssl-mode=DISABLED --target-dir=$TARGET_DIR
 echo "xtrabackup备份完毕."
